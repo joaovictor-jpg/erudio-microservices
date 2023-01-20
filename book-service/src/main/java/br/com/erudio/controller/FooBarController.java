a873590e1b1ad218da7eedb6cc798f0f8e916b5a
joao victor <joaovictormdasilva676@gmail.com>
2023-01-20 20:39:53 -0300

    Circuit Breaker with Resiliencs4j(Hystrix)

---
+++ book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -0,0 +1,35 @@
+package br.com.erudio.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+	
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+	
+	@GetMapping("/foo-bar")
+	// @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	// @CircuitBreaker(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	// @RateLimiter(name = "defaul")
+	@Bulkhead(name = "defaul")
+	public String fooBar() {
+		logger.info("Request to foo-bar is received");
+		// var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+		return "Foo-Bar!!!"; 
+		// return response.getBody();
+	}
+	
+	public String fallbackMethod(Exception ex) {
+		return "fallbackMethod foo-bar";
+	}
+
+}
