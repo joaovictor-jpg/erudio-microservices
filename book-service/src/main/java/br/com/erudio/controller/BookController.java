caa46b13ad09a48ad0b37587620f3e435a9a7bbf
joao victor <joaovictormdasilva676@gmail.com>
2023-01-19 22:18:56 -0300

    Load Balancing com Eureka, Feign e spring cloud LoadBalancer(Ribbon)

--- book-service/src/main/java/br/com/erudio/controller/BookController.java
+++ book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -36,7 +36,7 @@ public class BookController {
 		
 		var port = environment.getProperty("local.server.port");
 		
-		book.setEnvironment(port + " FEIGN");
+		book.setEnvironment("Book port: " + port + " Cambio port: " + cambio.getEnvironment());
 		book.setPrice(cambio.getConvertedValue());
 		
 		return book;
