7e2cd54bf28140133a5d2ea9cd3c668b4e05a545
joao victor <joaovictormdasilva676@gmail.com>
2023-02-05 00:14:38 -0300

    Distributed Tracing with Docker, Zipkin with Brave, Eureka and Sleuth

--- cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
@@ -3,6 +3,8 @@ package br.com.erudio.controller;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+	
+	private Logger logger = LoggerFactory.getLogger(CambioController.class);
 
     @Autowired
     private Environment environment;
@@ -30,6 +34,8 @@ public class CambioController {
     @GetMapping(value = "/{amout}/{from}/{to}")
     public Cambio getCambio(@PathVariable(value = "amout")BigDecimal amout, @PathVariable(value = "from") String from,
                             @PathVariable(value = "to") String to ) {
+    	
+    	logger.info("getCambio is called with -> {}, {} and {}", amout, from, to);
         
         var cambio = repository.findByFromAndTo(from, to);
         if(cambio == null) {
